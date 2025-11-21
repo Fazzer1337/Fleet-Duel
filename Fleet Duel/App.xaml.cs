b8313dbe3369bf0b13085ea55cf394e6aedf0bf9
Fazzer1337 <sudartem03@gmail.com>
2025-11-21 19:50:14 +1000

    Добавьте файлы проекта.

---
+++ Fleet Duel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Fleet_Duel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
